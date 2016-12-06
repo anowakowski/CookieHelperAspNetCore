cca40c27b5fea93455e983b764ff454187601cbf
Adrian Nowakowski <nowakowski.add@gmail.com>
2016-12-06 23:00:08 +0100

    change app routing.

--- CookieHelper/src/CookieHelper/Startup.cs
+++ CookieHelper/src/CookieHelper/Startup.cs
@@ -73,7 +73,7 @@ namespace CookieHelper
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Process}/{action=ProcessAction}/{id?}");
             });
         }
     }
